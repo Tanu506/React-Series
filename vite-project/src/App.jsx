5f817d748bdb97e3e22abb3e1df0a240f1e5e71d
Tanu Goyal <tanyagoyal258@gmail.com>
2026-09-20 20:12:07 +0530

    changes in vite project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,14 @@
+import Chai from "./chai"
 function App() {
 
   return (
-    <h1>Chai aur react vite </h1>
+    //first method - // <h1>Chai aur react vite </h1>
+    //second method - // <Chai/>
+    <>
+    <Chai/>
+    <h1>Chai aur react</h1>
+    <p>Hello from react</p>
+    </>
   )
 }
-
 export default App
