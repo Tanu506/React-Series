31e21bb3f4bca6c9e6575e2bcef90fc296223849
Tanu Goyal <tanyagoyal258@gmail.com>
2026-09-21 01:48:00 +0530

    Create my own react library and JSX

--- 01viteproject/src/App.jsx
+++ 01viteproject/src/App.jsx
@@ -1,12 +1,15 @@
 import Chai from "./chai"
-function App() {
 
+function App() { 
+  const username = "chai aur code";
   return (
     //first method - // <h1>Chai aur react vite </h1>
     //second method - // <Chai/>
     <>
     <Chai/>
-    <h1>Chai aur react</h1>
+    {/* <h1>Chai aur react</h1> */}
+    <h1>Chai aur react {username}</h1>
+     {/* {username} - evaluated expression */}
     <p>Hello from react</p>
     </>
   )
--- 01viteproject/src/main.jsx
+++ 01viteproject/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Chai</h1>
+    </div>
+  );
+}
+
+/*
+const reactElement = {
+    type : 'a',
+    props : {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children : 'click me to visit google'
+} */
+
+const anotherElement = (
+  <a href="htps://google.com" target="_blank">Visit Google</a>
+);
+const anotherUser = "chai aur react"
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit page",
+  anotherUser
+);
+
+createRoot(document.getElementById("root")).render(
+  // <App/>
+  //  <MyApp/>
+  // reactElement - It is not working
+  reactElement
+  // anotherElement
+  // <App/>
+);
